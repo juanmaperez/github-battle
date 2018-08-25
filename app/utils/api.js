bf8b3e8dacfdf19623a0e4acb176d0a15e723a9b
juanmaperez <juanmaperezvar@gmail.com>
2018-08-25 13:58:10 +0100

    migrate api.js to ES6

--- app/utils/api.js
+++ app/utils/api.js
@@ -2,31 +2,23 @@ const axios = require('axios');
 
 const id = 'YOUR_CLIENT_ID';
 const secret = 'YOUR_SECRET_ID';
-const params = '?client_id=' + id + '&client_secret=' + secret;
+const params = `?client_id=${id}&client_secret=${secret}`;
 
 function getProfile(username){
-  return axios.get('https://api.github.com/users/' + username + params)
-    .then((user)=>{
-      return user.data
-    })
+  return axios.get(`https://api.github.com/users/${username}${params}`)
+    .then(({data})=> data )
 }
 
 function getRepos(username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
-  
+  return axios.get(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
 }
 
 function getStartCount(repos) {
-  return repos.data.reduce((count, repo)=>{
-    return count + repo.stargazers_count;
-  }, 0)
+  return repos.data.reduce((count, {stargazers_count}) => count + stargazers_count, 0)
 }
 
-function calculateScore(profile, repos){
-  const followers = profile.followers;
-  const totalStars = getStartCount(repos);
-
-  return (followers * 3) + totalStars + repos.data.length;
+function calculateScore({followers}, repos){
+  return (followers * 3) + getStartCount(repos) + repos.data.length;
 }
 
 function handleError(error){
@@ -35,36 +27,28 @@ function handleError(error){
 }
 
 function getUserData(player){
-  return axios.all([
+  return Promise.all([
     getProfile(player),
     getRepos(player)
-  ]).then((data)=>{
-    const profile = data[0];
-    const repos = data[1];
-    return{
-      profile: profile,
+  ]).then(([profile, repos])=>({
+      profile,
       score: calculateScore(profile, repos)
-    }
-  })
+    })
+  )
 }
 
 function sortPlayers(players){
-  return players.sort((a, b)=>{
-    return b.score - a.score
-  })
+  return players.sort((a, b)=> b.score - a.score)
 }
 
 module.exports = {
-  battle: function (players) {
-    return axios.all(players.map(getUserData))
+  battle (players) {
+    return Promise.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError)
   },
-  fetchPopularRepos : function (language) {
-    const encodeURI = window.encodeURI('https://api.github.com/search/repositories?q=starts>1+language:' + language + '&sort=starts&order=desc&type0Repositories');
-    return axios.get(encodeURI)
-      .then((res)=> {
-        return res.data.items;
-      })
+  fetchPopularRepos (language) {
+    const encodeURI = window.encodeURI(`https://api.github.com/search/repositories?q=starts>1+language:${language}&sort=starts&order=desc&type0Repositories`);
+    return axios.get(encodeURI).then(({data})=> data.items)
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
-	entry: './app/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index_bundle.js',
-		publicPath: '/'
-	},
-	module: {
-		rules: [
-			{ test: /\.(js)$/, use: 'babel-loader' },
-			{ test: /\.css$/, use: ['style-loader', 'css-loader']},
-		]
-	},
-	devServer: {
-		historyApiFallback: true
-	},
-	plugins: [ 
-		new HtmlWebpackPlugin({
-				template: 'app/index.html'
-		})
-	],
-	mode: "development"
+var config = {
+  entry: ['babel-polyfill', './app/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
+
+module.exports = config;
